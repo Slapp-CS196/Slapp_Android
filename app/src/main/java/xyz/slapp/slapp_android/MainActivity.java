9c24e03a7cc4ffddd0ff84fcea234d7deb6a9512
shreyas <shreyas208@gmail.com>
2015-09-20 21:08:27 -0500

    + basic z-acceleration recognition

--- app/src/main/java/xyz/slapp/slapp_android/MainActivity.java
+++ app/src/main/java/xyz/slapp/slapp_android/MainActivity.java
@@ -1,5 +1,6 @@
 package xyz.slapp.slapp_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent i = new Intent(MainActivity.this, GestureTestActivity.class);
+        startActivity(i);
     }
 
     @Override
